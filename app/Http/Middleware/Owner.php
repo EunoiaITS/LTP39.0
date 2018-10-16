1e672e9ae70fc456a09f1dfaec2eff08cd1a02e7
Toki EIS <35416718+Toki-EIS@users.noreply.github.com>
2018-10-16 14:37:57 +0600

    auth implemented

--- app/Http/Middleware/Owner.php
+++ app/Http/Middleware/Owner.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Auth;
 
 class Owner
 {
@@ -15,6 +16,9 @@ class Owner
      */
     public function handle($request, Closure $next)
     {
-        return $next($request);
+        if(Auth::check() && Auth::user()->role == 'owner' || Auth::user()->role == 'dev'){
+            return $next($request);
+        }
+        abort(404);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,83 +11,93 @@
 |
 */
 
-Route::get('/','Owner@dashboard');
+Route::group(['middleware' => ['auth', 'owner']], function(){
 
-/**
- * client routes
- **/
+    Route::get('/','Owner@dashboard');
 
-Route::get('/create-client', 'Owner@createClient');
-Route::post('/create-client', 'Owner@createClient');
-Route::get('/clients-list', 'Owner@allClients');
-Route::post('/clients-list', 'Owner@allClients');
-Route::get('/client-details', 'Owner@clientDetails');
-Route::post('/client-details', 'Owner@clientDetails');
+    /**
+     * client routes
+     **/
 
-/**
-   *device routes
- **/
+    Route::get('/create-client', 'Owner@createClient');
+    Route::post('/create-client', 'Owner@createClient');
+    Route::get('/clients-list', 'Owner@allClients');
+    Route::post('/clients-list', 'Owner@allClients');
+    Route::get('/client-details', 'Owner@clientDetails');
+    Route::post('/client-details', 'Owner@clientDetails');
 
-Route::get('/create-device', 'Owner@createDevice');
-Route::post('/create-device', 'Owner@createDevice');
-Route::get('/manage-device', 'Owner@manageDevice');
-Route::post('/manage-device', 'Owner@manageDevice');
-Route::post('/delete-device', 'Owner@deleteDevice');
+    /**
+     *device routes
+     **/
 
-/**
- *billing routes
- **/
+    Route::get('/create-device', 'Owner@createDevice');
+    Route::post('/create-device', 'Owner@createDevice');
+    Route::get('/manage-device', 'Owner@manageDevice');
+    Route::post('/manage-device', 'Owner@manageDevice');
+    Route::post('/delete-device', 'Owner@deleteDevice');
 
-Route::get('/create-billing','Owner@createBilling');
-Route::post('/create-billing','Owner@createBilling');
-Route::get('/manage-billing','Owner@manageBilling');
-Route::post('/manage-billing','Owner@manageBilling');
-Route::get('/manage-billing-details','Owner@manageBillingDetails');
+    /**
+     *billing routes
+     **/
 
-/**
- *payment routes
- **/
+    Route::get('/create-billing','Owner@createBilling');
+    Route::post('/create-billing','Owner@createBilling');
+    Route::get('/manage-billing','Owner@manageBilling');
+    Route::post('/manage-billing','Owner@manageBilling');
+    Route::get('/manage-billing-details','Owner@manageBillingDetails');
 
-Route::post('/payment','Owner@payment');
+    /**
+     *payment routes
+     **/
 
-/**
- * settings
- * parking settings
-*/
+    Route::post('/payment','Owner@payment');
 
-Route::get('/settings/vehicle-types', 'Client@vehicleType');
-Route::post('/settings/vehicle-types', 'Client@vehicleType');
-Route::get('/settings/assign-parking', 'Client@assignParking');
-Route::post('/settings/assign-parking', 'Client@assignParking');
-Route::get('/settings/assign-rate', 'Client@assignRate');
-Route::post('/settings/assign-rate', 'Client@assignRate');
-Route::get('/settings/exempted-setting', 'Client@exemptedDuration');
-Route::post('/settings/exempted-setting', 'Client@exemptedDuration');
-Route::get('/settings/vat', 'Client@vat');
-Route::post('/settings/vat', 'Client@vat');
-Route::get('/settings/vip-parking', 'Client@vipParking');
-Route::post('/settings/vip-parking', 'Client@vipParking');
+});
 
-/**
- * Employee Routes
- */
+Route::group(['middleware' => ['auth', 'client']], function(){
 
-Route::get('/create-employee', 'Client@createEmployee');
-Route::post('/create-employee', 'Client@createEmployee');
-Route::get('/manage-employee', 'Client@manageEmployee');
-Route::post('/edit-password', 'Client@editPassword');
-Route::post('/edit-employee', 'Client@editEmployee');
-Route::post('/blocking', 'Client@blocking');
+    /**
+     * settings
+     * parking settings
+     */
+
+    Route::get('/settings/vehicle-types', 'Client@vehicleType');
+    Route::post('/settings/vehicle-types', 'Client@vehicleType');
+    Route::get('/settings/assign-parking', 'Client@assignParking');
+    Route::post('/settings/assign-parking', 'Client@assignParking');
+    Route::get('/settings/assign-rate', 'Client@assignRate');
+    Route::post('/settings/assign-rate', 'Client@assignRate');
+    Route::get('/settings/exempted-setting', 'Client@exemptedDuration');
+    Route::post('/settings/exempted-setting', 'Client@exemptedDuration');
+    Route::get('/settings/vat', 'Client@vat');
+    Route::post('/settings/vat', 'Client@vat');
+    Route::get('/settings/vip-parking', 'Client@vipParking');
+    Route::post('/settings/vip-parking', 'Client@vipParking');
+
+    /**
+     * Employee Routes
+     */
+
+    Route::get('/create-employee', 'Client@createEmployee');
+    Route::post('/create-employee', 'Client@createEmployee');
+    Route::get('/manage-employee', 'Client@manageEmployee');
+    Route::post('/edit-password', 'Client@editPassword');
+    Route::post('/edit-employee', 'Client@editEmployee');
+    Route::post('/blocking', 'Client@blocking');
+
+    /**
+     * Vip Routes
+     **/
+
+    Route::get('/vip-requests', 'Client@vipRequests');
+    Route::post('/create-vip', 'Client@createVip');
+    Route::get('/vip-list', 'Client@vipList');
+    Route::get('/vip-reject-list', 'Client@vipRejectList');
+
+});
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-/**
- * Vip Routes
- **/
 
-Route::get('/vip-requests', 'Client@vipRequests');
-Route::post('/create-vip', 'Client@createVip');
-Route::get('/vip-list', 'Client@vipList');
-Route::get('/vip-reject-list', 'Client@vipRejectList');
