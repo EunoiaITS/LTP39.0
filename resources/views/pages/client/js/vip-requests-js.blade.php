dc4dc09951454cb2462ec71952bcac50bcc5d3bb
Toki EIS <35416718+Toki-EIS@users.noreply.github.com>
2018-10-16 16:44:28 +0600

    ycu7tkyftdrsu6r tyu

--- resources/views/pages/client/js/vip-requests-js.blade.php
+++ resources/views/pages/client/js/vip-requests-js.blade.php
@@ -6,19 +6,19 @@
         $('.approve').on('click',function (e) {
            e.preventDefault();
            rel = $(this).attr('rel');
-           alert(rel);
-        });
-        $('#generate'+rel).on('click',function (e) {
-            e.preventDefault();
-            html = 'Id : '+$('#vip-id'+rel).val()+'<br/>' +
+            $('#generate'+rel).on('click',function (e) {
+                e.preventDefault();
+                alert(rel);
+                html = 'Id : '+$('#vip-id'+rel).val()+'<br/>' +
                 'Name : '+$('#name'+rel).val()+'<br/>'+
                 'Phone : '+$('#phone'+rel).val()+'<br/>';
-            $('#qr-code'+rel).qrcode(html);
-            var canvas = document.querySelector("#qr-code canvas");
-            var img = canvas.toDataURL("image/png");
-            var anc = '<a href="'+img+'" class="download-btn" target="_blank" download="qrcode.png"><i class="fas fa-arrow-alt-circle-down"></i></a>';
-            $('#qr-image').val(img);
-            $('#qr-code'+rel).append(anc);
+                $('#qr-code'+rel).qrcode(html);
+                var canvas = document.querySelector("#qr-code canvas");
+                var img = canvas.toDataURL("image/png");
+                var anc = '<a href="'+img+'" class="download-btn" target="_blank" download="qrcode.png"><i class="fas fa-arrow-alt-circle-down"></i></a>';
+                $('#qr-image').val(img);
+                $('#qr-code'+rel).append(anc);
+            });
         });
     });
 </script>
